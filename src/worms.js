0ffd4afbbe7bb397dcade21609b75b3f5205309f
Kat M <kmunson13@gmail.com>
2022-11-15 12:52:37 -0600

    Initial commit

--- src/worms.js
+++ src/worms.js
@@ -1,14 +1,21 @@
-const Worm = ({length}) => {
+const Worm = (length) => {
+    //set doesn't allow duplicate values, good for hits functionality
     const hits = new Set()
 
     const isEaten = () => {
         return hits.size === length
     }
 
+    const hit = (coordinates) => {
+        //add logic to see if hit coordinates don't already exist in set and are on valid place on board and ship location
+        hits.add(coordinates)
+    }
+
     return {
+        hit,
         length,
         isEaten,
-        hits: []
+        hits
     }
 }
 
